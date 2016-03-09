92cf4e6dbd4d3238cb36c3a79e3a1fb1f5bb5f64
Neil de Carteret <ndecarteret@sportingindex.com>
2016-03-09 09:43:19 +0000

    split out bundle-task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,5 @@
 import gulp from "gulp"
-import browserify from "browserify"
-import babelify from 'babelify'
-import source from "vinyl-source-stream"
-import buffer from "vinyl-buffer"
-import sourcemaps from "gulp-sourcemaps"
-import uglify from "gulp-uglify"
-import watchify from "watchify"
-import gutil from "gulp-util"
-import path from "path"
-import gulpIf from "gulp-if";
+import bundleTask from "./gulp-helpers/bundle-task"
 
 const _outFolder   = "__build"
 const _inFile = "src/project.js"
@@ -17,51 +8,12 @@ const _production = process.env.NODE_ENV === "production";
 
 gulp.task("default", ["html", "bundle"])
 
-
 gulp.task("html", function () {
   gulp.src("html/index.html").pipe(gulp.dest(_outFolder))
 })
 
-function bundleTask (inFile, outFile, outFolder, opts = {watch: false, production: false}) {
+gulp.task("bundle",
+  bundleTask(_inFile, _outFile, _outFolder, {production: _production}));
 
-  const bundler = (opts.watch ? watchify : (x) => x)(
-    browserify({debug: !opts.production}).add(inFile).plugin(babelify)
-  )
-
-  function bundle () {
-    return bundler.bundle()
-                  .pipe(source(outFile))
-                  .pipe(buffer())
-                  .pipe(gulpIf(!opts.production, sourcemaps.init({loadMaps: true})))
-                  .pipe(gulpIf(opts.production, uglify()))
-                  // write sourcemaps inline to prevent bug in Chrome
-                  // https://bugs.chromium.org/p/chromium/issues/detail?id=508270
-                  .pipe(gulpIf(!opts.production, sourcemaps.write()))
-                  .pipe(gulp.dest(outFolder));
-  }
-
-  bundler.on("update", function (ids) {
-    gutil.log("Script dependencies updated:");
-    ids = ids.map(function (id) {
-      return gutil.colors.magenta(path.relative(process.cwd(), id));
-    });
-    ids.forEach(function (id) { gutil.log(id); });
-    bundle();
-  })
-
-  bundler.on("log", function (msg) {
-    gutil.log(msg);
-  })
-
-  return bundle()
-}
-
-
-
-gulp.task("bundle", function() {
-  return bundleTask(_inFile, _outFile, _outFolder, {production: _production})
-});
-
-gulp.task("watch", ["bundle"], function() {
-  return bundleTask(_inFile, _outFile, _outFolder, {production: _production, watch: true})
-});
+gulp.task("watch", ["bundle"],
+  bundleTask(_inFile, _outFile, _outFolder, {production: _production, watch: true}));
